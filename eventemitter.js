468c2f28cb6291feb17896c110e83f98dc1004c6
Adametsofficial <av.styledev@gmail.com>
2020-12-06 20:26:21 +0200

    feet: add addListener method

--- eventemitter.js
+++ eventemitter.js
@@ -2,4 +2,15 @@ class EventEmitter {
     constructor() {
         this.events = Object.create({});
     }
+
+    on(eventName, ...callbacks) {
+        if (!callbacks) {
+            (this.events[eventName] = []);
+            
+            return;
+        }
+
+        this.events[eventName] = [...callbacks];
+    }
+
 }
