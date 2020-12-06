dced94c83df9e641940c98bdf891b27e87230059
Adametsofficial <av.styledev@gmail.com>
2020-12-06 20:27:03 +0200

    feet: add 'emit' method

--- eventemitter.js
+++ eventemitter.js
@@ -6,11 +6,22 @@ class EventEmitter {
     on(eventName, ...callbacks) {
         if (!callbacks) {
             (this.events[eventName] = []);
-            
+
             return;
         }
 
         this.events[eventName] = [...callbacks];
     }
 
+    emit(eventName, ...args) {
+        return this.events &&
+        this.events[eventName].length &&
+        this.events[eventName].forEach((fn) => {
+            args.length
+                ? args.forEach((arg) => {
+                    fn(arg);
+                })
+                : fn(undefined);
+        });
+    }
 }
